c59ad96863225288f716c94f53a9e9eed2fdc05d
Sebastian Moors <mauser@smoors.de>
2017-03-11 11:10:33 +0100

    Added drumkit xml file (Fileending php is meaningless, only to keep the entry in the server list consistent)

---
+++ feeds/drumkit_list.php
@@ -0,0 +1,287 @@
+<?xml version='1.0' encoding='UTF-8'?><drumkit_list>
+<drumkit>
+<name>Audiophob</name>
+<url>http://hydrogen-music.org/drumkits/Audiophob.h2drumkit</url>
+<info>Cheap sounds from freesound.org with no copyright</info>
+<author>soundcloud.com/audiophobdubstep</author>
+<license>This work is licensed under the Creative Commons 0 License.</license>
+</drumkit>
+
+<drumkit>
+                <name>belofilms.com - AC-Guitar-Strums (flac)</name>
+                <url>http://hydrogen-music.org/drumkits/belofilms_GuitarStrums.h2drumkit</url>
+                <info>
+                &lt;!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+                &lt;html>&lt;head>&lt;meta name="qrichtext" content="1" />&lt;style type="text/css">
+                p, li { white-space: pre-wrap; }
+                &lt;/style>&lt;/head>&lt;body style=" font-family:'Lucida Grande'; font-size:10pt; font-weight:400; font-style:normal;">
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Part of &amp;quot;the one man movie proyect&amp;quot;, please visit www.belofilms.com  for more info.&lt;/p>
+                &lt;p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Made with my own samples, 4 layers - 96khz - 24bit - Stereo&lt;/p>
+                &lt;p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">*Feel free to use it for commercial music, as long as you give the proper noticeable credit: Gabriel Verdugo Soto (www.belofilms.com)&lt;/p>
+                &lt;p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">******* Please read the drumkit guidelines up and below *******&lt;/p>
+                &lt;p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Order example: Gminor / F2&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Gminor: Chord name.&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">/: Slide up (/), down (\) or no slide ().&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">F2: Major key and position inside that scale.&lt;/p>
+                &lt;p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">&lt;/p>
+                &lt;p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">**If you want to have scales different than C you have to rearranged the order of the instruments according to the major keys information available in each instrument.&lt;/p>&lt;/body>&lt;/html></info>
+                <author>Gabriel Verdugo Soto (belo@belofilms.com)</author>
+		<license>CC-BY 3.0 Unported</license>
+</drumkit>
+
+<drumkit>
+<name>BJA_Pacific</name>
+<url>http://sourceforge.net/projects/hydrogen/files/Sound%20Libraries/Main%20sound%20libraries/BJA_Pacific.h2drumkit</url>
+<info>Multi-layered drumkit, based on samples from Bransin Anderson </info>
+<author>Luc Tunguay</author>
+<license>Attribution-Share Alike 3.0 United States</license>
+</drumkit>
+
+
+
+<drumkit><name>Boss DR-110 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Boss_DR-110.h2drumkit</url><info>A compact programmable drum machine, DR-110 is one of the last fully-analog computer-controlled boxes made by Roland/Boss. It's ever-fresh sounds lie somewhere between TR-808 and TR-606.</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>Classic 3355606 (sf)</name><url>http://prdownloads.sf.net/hydrogen/3355606kit.h2drumkit</url><info>Based on sounds from famous and classic beatboxes of 1970s and 1980s</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>Classic 626 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Classic-626.h2drumkit</url><info>One of the very last drum boxes of the TR series. TR-626 featured 30 12-bit PCM samples of classic drum and percussion sounds, as well as latin percussion.</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit>
+<name>circAfrique v4</name>
+<url>http://sourceforge.net/projects/hydrogen/files/Sound%20Libraries/Main%20sound%20libraries/circAfrique v4.h2drumkit</url>
+<info>A collection of african instruments from  www.circAfrique.com. Includes djembe, sangban, dununba and kenkeni.</info>
+<author>Jim Harney</author>
+<license>Creative Commons - Attribution-ShareAlike 3.0 Unported</license>
+</drumkit>
+
+<drumkit><name>ColomboAcousticDrumkit (sf)</name><url>http://prdownloads.sf.net/hydrogen/ColomboAcousticDrumkit.h2drumkit</url><info>Based on "Colombo Percussion", handmade in Argentina circa 1970 to 1980.
+* Wooden toms, snare and bassdrum
+* Toms: 12X8 13X9 16X16 (hi, mid, low-foor)
+* Bassdrum: 21X14
+* Snare "piccolo" 14X4
+* Aquarian patch set
+* Crash 16'' Sabian "B8 Pro, Thin Crash"
+* Crash 20'' Paiste 1000 "Power Crash 20''", made in Switzerland
+* Hihat Paiste 1000 14'' "Rude", made in Switzerland</info><author>(c)2006 by Marcos Guglielmetti</author><license></license></drumkit><drumkit><name>YamahaVintageKit (sf)</name><url>http://prdownloads.sf.net/hydrogen/YamahaVintageKit.h2drumkit</url><info>Pristine-quality, stereo multi-layered kit based on samples of a 1970's Yamaha drum kit.</info><author>Artemiy Pavlov</author><license></license>
+    </drumkit>
+	
+	
+
+<drumkit><name>DeathMetal (sf)</name><url>http://prdownloads.sf.net/hydrogen/DeathMetal.h2drumkit</url><info>This drumkit was created and published for all the people who want to make some metal drum patterns to sound more natural.</info><author>Archman</author><license>CC-Attribution</license></drumkit>
+
+<drumkit>
+<name>Denon CRB-90</name>
+<url>http://hydrogen-music.org/drumkits/Denon CRB-90.h2drumkit</url>
+<info>This drumkit was created with samples from the original Denon CRB-90 Rhythm Box.  More info can be found on http://audio-and-linux.blogspot.com/2012/06/denon-rhythm-box-for-hydrogen.htm</info>
+<author>Thijs Van Severen</author>
+<license>CC-BY-SA</license>
+</drumkit>
+
+<drumkit>
+<name>Drumkit excepcional</name>
+<url>http://hydrogen-music.org/drumkits/Drumkit excepcional.h2drumkit</url>
+<info>Sons gravados de maneira caseira</info>
+<author>Pedro MaurÃ­cio</author>
+<license>creative commons</license>
+</drumkit>
+
+
+<drumkit><name>EasternHop (sf)</name><url>http://prdownloads.sf.net/hydrogen/EasternHop-1.h2drumkit</url><info>A perfect kit for ethnic hip-hop: contains groovy kicks, snares and hats along with indian tabla drums</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>Electric Empire (sf)</name><url>http://prdownloads.sf.net/hydrogen/ElectricEmpireKit.h2drumkit</url><info>World-class original synth drum and percussion sounds, all painstakingly synthesised from noise and sine waves. Perfect for any sort of electro, old-school hip hop and break beat.</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>Erny's percussions (sf)</name><url>http://prdownloads.sf.net/hydrogen/ErnysPercussion.h2drumkit</url><info>A nice contributed drumkit with 32 percussion instruments.</info><author>Erny</author><license></license></drumkit>
+
+<drumkit>
+<name>Forzee Stereo Drumkit</name>
+<url>https://sourceforge.net/projects/hydrogen/files/Sound%20Libraries/Main%20sound%20libraries/ForzeeStereo.h2drumkit</url>
+<info>
+This drumkit includes:
+  - TAMA Superstar Drum set (Kick, Tom Low, Tom Mid, Tom High)
+  - Pearl Snare, Tambourine, Agogo
+  - Paiste Cymbals
+  - Custom Ride (Hand-made tuning)
+   
+All drums were recorded with 4 microphones:
+     - Kick drum: Shure BETA 52 A, Shure SM 57, 2 x MXL 606
+     - All others: 2 x Shure SM 57, 2 x MXL 606
+   
+All recorded data combined into stereo tracks.
+No equalization was applied for better use.
+Please enjoy them.
+   
+Please visit http://forzee.org/ for contacts.
+
+</info>
+<author>Vladimir Sadovnikov, Alexander Belokurov</author>
+<license>This drumkit is distributed under the GPL version 2 license.</license>
+</drumkit>
+
+
+
+<drumkit>
+<name>Gimme A Hand 1.0</name>
+<url>http://hydrogen-music.org/drumkits/Gimme A Hand 1.0.h2drumkit</url>
+<info>An Assortment of Hand Percussion Instruments Including Cajon, Bongos, Maracas and Much More...</info>
+<author>Glen MacArthur</author>
+<license>GPL</license>
+</drumkit>
+
+<drumkit>
+<name>GSCW Kit 1 (Flac edition)</name>
+<url>http://www.hydrogen-music.org/drumkits/Flac_GSCW-1.h2drumkit</url>
+<info> The flac edition of the famous GSCW drumkit. Some layers were removed to reduce the size. Based on samples from Salvador Pelaez</info>
+<author>Stefano Carbonelli</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit>
+<name>GSCW Kit 2 (Flac edition)</name>
+<url>http://www.hydrogen-music.org/drumkits/Flac_GSCW-2.h2drumkit</url>
+<info> The flac edition of the famous GSCW drumkit. Some layers were removed to reduce the size. Based on samples from Salvador Pelaez</info>
+<author>Stefano Carbonelli</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit><name>HardElectro (sf)</name><url>http://prdownloads.sf.net/hydrogen/HardElectro1.h2drumkit</url><info></info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>HipHop-1 (sf)</name><url>http://prdownloads.sf.net/hydrogen/HipHop-1.h2drumkit</url><info></info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>HipHop-2 (sf)</name><url>http://prdownloads.sf.net/hydrogen/HipHop-2.h2drumkit</url><info></info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit>
+<name>K-27 Trash Kit</name>
+<url>https://sourceforge.net/projects/hydrogen/files/Sound%20Libraries/Main%20sound%20libraries/K-27_Trash_Kit.h2drumkit</url>
+<info>The K-27 Trash Kit is a cool set to compose punk and garage musik</info>
+<author>Wolke</author>
+</drumkit>
+
+
+<drumkit>
+<name>Lightning1024</name>
+<url>http://hydrogen-music.org/drumkits/Lightning1024.h2drumkit</url>
+<info>An small library made by me to get a clear sound in the most posible lightweight bank. Una pequeña libreria hecha por mi para obtener un sonido claro en un banco lo mas livianamente posible.</info>
+<author>Esteban Mariano Romanelli</author>
+<license>Free!!!</license>
+</drumkit>
+
+
+<drumkit><name>Millo drums 1 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Millo-Drums_v.1.h2drumkit</url><info>A nice kit from Emillo (Cricket studio)</info><author>Emiliano Grilli</author><license></license></drumkit>
+
+<drumkit><name>Millo's MultiLayered 2 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Millo_MultiLayered2.h2drumkit</url><info>Nice and multilayered drumkit from Emillo.net</info><author>Emiliano Grilli</author><license></license></drumkit>
+
+<drumkit><name>Millo's MultiLayered 3 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Millo_MultiLayered3.h2drumkit</url><info>This is the third version of a ludwig drumkit. Recorded at cricketstudios.it, released under the GPL. Feel free to modify it and integrate it with other sounds. If you end up with good sounding demos with it, please mail them to emillo@emillo.net</info><author>Emiliano Grilli</author><license></license></drumkit>
+
+<!-- Links seem to be dead by now..
+<drumkit>
+<name>Roland TR-909</name>
+<url>http://www.hydrogen-music.org/download/drumkits/Roland TR-909.h2drumkit</url>
+<info></info>
+<author>Artemiy Pavlov</author>
+</drumkit>
+
+<drumkit>
+<name>Roland TR-808</name>
+<url>http://www.hydrogen-music.org/download/drumkits/Roland TR-808.h2drumkit</url>
+<info></info>
+<author>Artemiy Pavlov</author>
+</drumkit>
+
+<drumkit>
+<name>Roland TR-707</name>
+<url>http://www.hydrogen-music.org/download/drumkits/Roland TR-707.h2drumkit</url>
+<info></info>
+<author>Artemiy Pavlov</author>
+</drumkit>
+
+<drumkit>
+<name>Roland TR-606</name>
+<url>http://www.hydrogen-music.org/download/drumkits/Roland TR-606.h2drumkit</url>
+<info></info>
+<author>Artemiy Pavlov</author>
+</drumkit>
+-->
+
+<drumkit>
+<name>Roland_MC-307_CR78&amp;Cheaps</name>
+<url>http://hydrogen-music.org/drumkits/Roland_MC-307_CR78&amp;Cheaps.h2drumkit</url>
+<info> An CR78&amp;Cheaps MC-307 groovebox clone</info>
+<author>Michael Wolkstein</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit>
+<name>Roland_MC-307_TR-606</name>
+<url>http://hydrogen-music.org/drumkits/Roland_MC-307_TR-606.h2drumkit</url>
+<info> An TR-606 MC-307 groovebox clone</info>
+<author>Michael Wolkstein</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit>
+<name>Roland_MC-307_TR-808_</name>
+<url>http://hydrogen-music.org/drumkits/Roland_MC-307_TR-808.h2drumkit</url>
+<info> An TR-808 MC-307 groovebox clone</info>
+<author>Michael Wolkstein</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit>
+<name>Roland_MC-307_TR-909</name>
+<url>http://hydrogen-music.org/drumkits/Roland_MC-307_TR-909.h2drumkit</url>
+<info> An TR-909 MC-307 groovebox clone</info>
+<author>Michael Wolkstein</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit>
+<name>Roland_MC-307_Techno1</name>
+<url>http://hydrogen-music.org/drumkits/Roland_MC-307_Techno1.h2drumkit</url>
+<info> An Techno1 MC-307 groovebox clone</info>
+<author>Michael Wolkstein</author>
+<license>Do not redistribute or publish the inluded samples.</license>
+</drumkit>
+
+<drumkit>
+<name>rumpf_kit_z01_gm</name>
+<url>http://hydrogen-music.org/drumkits/rumpf_kit_z01_h2.h2drumkit</url>
+<info>Drum kit from analog samples.  Partly GM-drum compatible.  Format:48000Hz,24Bit,flac</info>
+<author>Emanuel Rumpf</author>
+<license>cc-by ==> http://creativecommons.org/licenses/by/3.0/</license>
+</drumkit>
+
+
+
+<drumkit><name>Synthie-1 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Synthie-1.h2drumkit</url><info></info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>TD-7 (sf)</name><url>http://prdownloads.sf.net/hydrogen/TD-7kit.h2drumkit</url><info>A kit based on samples from Roland TD-7 percussion sound module of middle 1990s</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit><name>Techno-1 (sf)</name><url>http://prdownloads.sf.net/hydrogen/Techno-1.h2drumkit</url><info></info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+<drumkit>
+<name>The Black Pearl 1.0</name>
+<url>http://hydrogen-music.org/drumkits/The Black Pearl 1.0.h2drumkit</url>
+<info>A Sampled 5pc Pearl DX Series Drumkit.</info>
+<author>Glen MacArthur</author>
+<license>GPL</license>
+</drumkit>
+
+
+<drumkit><name>TR808909 (sf)</name><url>http://prdownloads.sf.net/hydrogen/TR808909.h2drumkit</url><info></info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+
+<drumkit><name>VariBreaks (sf)</name><url>http://prdownloads.sf.net/hydrogen/VariBreaks.h2drumkit</url><info>All sounds in this kit have been created from scratch using Roland V-Synth. The source material were samples of sounds based on sine and noise waveforms processed with COSM blocks, which were then re-synthesized multiple times using VariPhrase and COSM technologies.</info><author>Artemiy Pavlov</author><license></license></drumkit>
+
+
+
+
+	<pattern>
+		<name>JazzRockFast</name>
+		<author>Benoit Rouits</author>
+		<license>Public Domain</license>
+		<url>http://hydrogen-music.org/songs_patterns/Patterns/JazzRockFast.h2pattern</url>
+	</pattern>
+</drumkit_list>
